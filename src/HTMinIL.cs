6d6a7ab6b0f78fc1de1a929260a86a9e57df48b9
Rodol Phito <rodolvertice@gmail.com>
2019-07-10 22:59:44 -0700

    Document assembly processing.

--- src/HTMinIL.cs
+++ src/HTMinIL.cs
@@ -5,11 +5,16 @@ using Mono.Cecil.Cil;
 
 public class HTMinIL
 {
+	// Regexes to perform the HTML minification
 	Regex flattenSpaces = new Regex(@"\s+", RegexOptions.Compiled);
 	Regex collapseBracket = new Regex(@"(?:(?<=^|>) (?=<))|(?:(?<=>) (?=<|$))", RegexOptions.Compiled);
 	Regex selfClosingSpace = new Regex(@" (?=/>)", RegexOptions.Compiled);
 	Regex htmlComments = new Regex(@"<!--(?!\[if ).*(?<!\[endif\])-->", RegexOptions.Compiled | RegexOptions.Singleline);
+
+	// Instruction cache to keep track of empty WriteLiteral calls
 	List<Instruction> toBeRemoved = new List<Instruction>();
+
+	// Statistics to print a summary of the minification
 	ulong totalCharactersInput = 0;
 	ulong totalCharactersOutput = 0;
 	ulong eliminatedFunctions = 0;
@@ -19,17 +24,26 @@ public class HTMinIL
 	{
 		ModuleDefinition modDef = asmDef.MainModule;
 
+		// *.Views.dll have all the views as top level types
 		foreach (TypeDefinition type in modDef.Types)
 		{
+			// The ExecuteAsync method is async, so it generates a nested type
 			foreach (TypeDefinition nestedType in type.NestedTypes)
 			{
+				// Only process the generated nested types,
+				// because there may be legitimate user-made nested types too.
 				if (!nestedType.Name.Contains("ExecuteAsync")) continue;
-				processedFiles++;
+
+				// Every method in the nested type must be processed,
+				// since every await splits the method further.
 				foreach (MethodDefinition method in nestedType.Methods)
 				{
 					ProcessMethod(method);
 				}
 			}
+
+			// Keep track of how many views are processed for statistics.
+			processedFiles++;
 		}
 	}
 
